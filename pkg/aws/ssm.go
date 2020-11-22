f7b06df565ab565d1936372109ed43810df78d1b
Fabio Gollinucci <fbgolly@gmail.com>
2020-11-22 16:11:14 +0100

    Fix redundant declaration

--- pkg/aws/ssm.go
+++ pkg/aws/ssm.go
@@ -133,7 +133,7 @@ func SSMListDocuments(ses *session.Session, owner string) ([]SSMDocument, error)
 	var documents []SSMDocument
 	ssmSvc.ListDocumentsPages(&ssm.ListDocumentsInput{
 		Filters: []*ssm.DocumentKeyValuesFilter{
-			&ssm.DocumentKeyValuesFilter{
+			{
 				Key: aws.String("Owner"),
 				Values: aws.StringSlice([]string{
 					owner,
